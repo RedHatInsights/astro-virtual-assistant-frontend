cc38b8f41508309f556f1f3474209f24378ea25a
Justin Orringer <justinorringer@gmail.com>
2023-10-31 11:05:49 -0400

    Remove sad bubble if message content

--- src/Components/Message/AssistantMessageEntry.tsx
+++ src/Components/Message/AssistantMessageEntry.tsx
@@ -14,18 +14,20 @@ const OPTION_COLORS = ['blue', 'cyan', 'green', 'orange', 'purple', 'grey', 'gol
 export const AssistantMessageEntry: FunctionComponent<AssistantMessageProps> = ({ message, ask }) => {
   return (
     <>
-      <Split className="astro-chatbot">
-        <SplitItem>
-          <Icon size="lg" className="pf-u-mr-sm">
-            <RedHatIcon />
-          </Icon>
-        </SplitItem>
-        <SplitItem className="bubble pf-u-background-color-200">
-          <TextContent className="pf-u-font-size-sm">
-            <ReactMarkdown>{message.content}</ReactMarkdown>
-          </TextContent>
-        </SplitItem>
-      </Split>
+      {message.content && (
+        <Split className="astro-chatbot">
+          <SplitItem>
+            <Icon size="lg" className="pf-u-mr-sm">
+              <RedHatIcon />
+            </Icon>
+          </SplitItem>
+          <SplitItem className="bubble pf-u-background-color-200">
+            <TextContent className="pf-u-font-size-sm">
+              <ReactMarkdown>{message.content}</ReactMarkdown>
+            </TextContent>
+          </SplitItem>
+        </Split>
+      )}
 
       {message.options && (
         <div className="astro-chatbot pf-u-pl-xl">
