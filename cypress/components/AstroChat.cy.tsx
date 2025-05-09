67efe6107ddcd7bc20741fd7ad170fe447653e4d
Justin Orringer <justinorringer@gmail.com>
2025-05-09 15:08:25 -0400

    Command test and general chat test

--- cypress/components/AstroChat.cy.tsx
+++ cypress/components/AstroChat.cy.tsx
@@ -1,23 +1,91 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { AstroChat } from '../../src/v2/Components/AstroChat/AstroChat';
+import { From, Message } from '../../src/v2/types/Message';
 
-describe('AstroChat', () => {
+const basicMessages: Message[] = [
+  {
+    from: From.USER,
+    content: 'Hello, how can I help you?',
+  },
+  {
+    from: From.ASSISTANT,
+    content: 'I am here to assist you.',
+    messageId: '1',
+    isLoading: false,
+  },
+  {
+    from: From.ASSISTANT,
+    content: 'I am here to assist you.',
+    messageId: '1',
+    isLoading: true,
+  },
+  {
+    from: From.ASSISTANT,
+    content: 'Here\'s what I can do',
+    messageId: '1',
+    isLoading: false,
+    options: [
+      {
+        value: 'option1',
+        text: 'Option 1',
+        optionId: '1',
+      },
+      {
+        value: 'option2',
+        text: 'Option 2',
+        optionId: '2',
+      },
+    ]
+  }
+];
+
+const AstroChatComponent = ({ m }: { m: Message[] }) => {
+  const [messages, setMessages] = useState<Message[]>(m);
+  const [isLoading, setIsLoading] = useState(false);
+  const [fullscreen, setFullscreen] = useState(false);
+
+  const ask = async (what: string) => {};
+
+  return (
+    <BrowserRouter>
+      <AstroChat
+        messages={messages}
+        setMessages={setMessages}
+        ask={ask}
+        preview={false}
+        onClose={() => {}}
+        blockInput={false}
+        fullscreen={fullscreen}
+        setFullScreen={setFullscreen}
+        isLoading={isLoading}
+      />
+    </BrowserRouter>
+  );
+}
+
+describe('Basic chat test', () => {
   it('renders without crashing', () => {
     cy.mount(
-      <BrowserRouter>
-        <AstroChat
-          messages={[]}
-          setMessages={() => {}}
-          ask={() => Promise.resolve()}
-          preview={false}
-          onClose={() => {}}
-          blockInput={false}
-          fullscreen={false}
-          setFullScreen={() => {}}
-          isLoading={false}
-        />
-      </BrowserRouter>
+      <AstroChatComponent
+        m={[]}
+      />
     );
+  });
+
+  it('loads messages', () => {
+    cy.mount(
+      <AstroChatComponent
+        m={basicMessages}
+      />
+    );
+    // need better selectors
+    cy.get('.user-0-2-11 > .pf-v5-l-split__item > .pf-v5-c-content').contains('Hello, how can I help you?');
+    cy.get('p').contains('I am here to assist you.');
+    cy.get('.typing-0-2-22').should('exist');
+
+    // check buttons
+    cy.contains('Option 1').should('exist');
+    cy.contains('Option 2').should('exist');
   });
 });
---
+++ cypress/components/CommandMessageProcessor.cy.tsx
@@ -0,0 +1,211 @@
+import ScalprumProvider from '@scalprum/react-core';
+import { commandMessageProcessor } from '../../src/v2/SharedComponents/AstroVirtualAssistant/CommandMessageProcessor';
+import { CommandType } from '../../src/v2/types/Command';
+import { AssistantMessage, From } from '../../src/v2/types/Message';
+import React, { useEffect } from 'react';
+
+
+const BASIC_MESSAGE : AssistantMessage = {
+  from: From.ASSISTANT,
+  content: null,
+  messageId: '1',
+  isLoading: false,
+};
+
+
+const CommandMessageProcessorWrapper: React.FC<{ message: AssistantMessage; options: any }> = ({ message, options }) => {
+  useEffect(() => {
+    commandMessageProcessor(message, options);
+  }, [message, options]);
+
+  return <div>CommandMessageProcessor Test</div>;
+};
+
+describe('Basic CommandMessageProcessor functions', () => {
+  let options: any;
+
+  beforeEach(() => {
+    // Mock options object
+    options = {
+      addSystemMessage: cy.stub(),
+      addBanner: cy.stub(),
+      toggleFeedbackModal: cy.stub(),
+      isPreview: false,
+    };
+  });
+
+  it('should handle FINISH_CONVERSATION command', async () => {
+    const message : AssistantMessage = {
+      from: From.ASSISTANT,
+      content: null,
+      command: {
+        type: CommandType.FINISH_CONVERSATION,
+        params: { args: [] },
+      },
+      messageId: '1',
+      isLoading: false,
+    };
+
+    await commandMessageProcessor(message, options);
+
+    expect(options.addSystemMessage).to.have.been.calledWith('finish_conversation_message', []);
+    expect(options.addBanner).to.have.been.calledWith('finish_conversation_banner', []);
+  });
+
+  it('should handle REDIRECT command with a valid URL', async () => {
+    const message = {
+      command: {
+        type: CommandType.REDIRECT,
+        params: { args: ['https://example.com'] },
+      },
+      ...BASIC_MESSAGE,
+    };
+
+    await commandMessageProcessor(message, options);
+
+    expect(options.addSystemMessage).to.have.been.calledWith('redirect_message', ['https://example.com']);
+  });
+
+  it('should log an error for REDIRECT command without a URL', async () => {
+    cy.spy(console, 'error').as('consoleError');
+
+    const message = {
+      command: {
+        type: CommandType.REDIRECT,
+        params: { args: [] },
+      },
+      ...BASIC_MESSAGE,
+    };
+
+    await commandMessageProcessor(message, options);
+
+    cy.get('@consoleError').should('be.calledWith', 'URL is required for redirect command');
+  });
+
+  it('should log an error for TOUR command with an unknown tour name', async () => {
+    cy.spy(console, 'error').as('consoleError');
+
+    const message = {
+      command: {
+        type: CommandType.TOUR,
+        params: { args: ['unknown'] },
+      },
+      ...BASIC_MESSAGE,
+    };
+
+    await commandMessageProcessor(message, options);
+
+    cy.get('@consoleError').should('be.calledWith', 'Unknown tour name: unknown');
+  });
+
+  it('should handle FEEDBACK_MODAL command', async () => {
+    const message = {
+      command: {
+        type: CommandType.FEEDBACK_MODAL,
+        params: { args: [] },
+      },
+      ...BASIC_MESSAGE,
+    };
+
+    await commandMessageProcessor(message, options);
+
+    expect(options.toggleFeedbackModal).to.have.been.calledWith(true);
+  });
+});
+
+describe('CommandMessageProcessors that call APIs', () => {
+  let options: any;
+  beforeEach(() => {
+    // Mock options object
+    options = {
+      addSystemMessage: cy.stub(),
+      addBanner: cy.stub(),
+      toggleFeedbackModal: cy.stub(),
+      isPreview: false,
+    };
+  });
+
+  // it('should handle MANAGE_ORG_2FA command successfully', async () => {
+  //   const message = {
+  //     command: {
+  //       type: CommandType.MANAGE_ORG_2FA,
+  //       params: { args: ['enable'] },
+  //     },
+  //     ...BASIC_MESSAGE,
+  //   };
+
+  //   await commandMessageProcessor(message, options);
+
+  //   expect(options.addBanner).to.have.been.calledWith('toggle_org_2fa', ['enable']);
+  // });
+
+  // it('should handle MANAGE_ORG_2FA command with failure', async () => {
+  //   cy.stub(window, 'fetch').rejects(new Error('Failed to toggle 2FA'));
+
+  //   const message = {
+  //     command: {
+  //       type: CommandType.MANAGE_ORG_2FA,
+  //       params: { args: ['enable'] },
+  //     },
+  //     ...BASIC_MESSAGE,
+  //   };
+
+  //   await commandMessageProcessor(message, options);
+
+  //   expect(options.addBanner).to.have.been.calledWith('toggle_org_2fa_failed', ['enable']);
+  // });
+
+  it('should handle CREATE_SERVICE_ACCOUNT command successfully', async () => {
+    cy.intercept('POST', 'https://sso.redhat.com/auth/realms/redhat-external/apis/service_accounts/v1', {
+      statusCode: 201,
+      body: {id: '12345', name: 'test-name', description: 'test description please', secret: 'secret'},
+    }).as('serviceAccountAPI');
+  
+    const message = {
+      command: {
+        type: CommandType.CREATE_SERVICE_ACCOUNT,
+        params: { args: ['test-name', 'test description please', 'prod'] },
+      },
+      ...BASIC_MESSAGE,
+    };
+
+    cy.mount(<ScalprumProvider
+        config={{ foo: { name: 'foo' } }}
+        api={{ 
+          chrome: {
+            auth: {
+              getUser: () => Promise.resolve({ identity: { user: { is_org_admin: true }} }), 
+              getToken: () => "token",
+            },
+          }
+        }}
+      >
+        <CommandMessageProcessorWrapper message={message} options={options} />
+      </ScalprumProvider>
+    );
+
+    expect(options.addBanner).to.have.been.calledWith('create_service_account', [
+      'test-name',
+      'test description please',
+      '12345',
+      'secret,'
+    ]);
+    cy.wait('@serviceAccountAPI');
+  });
+
+  // it('should handle CREATE_SERVICE_ACCOUNT command with failure', async () => {
+  //   cy.stub(window, 'fetch').rejects(new Error('Failed to create service account'));
+
+  //   const message = {
+  //     command: {
+  //       type: CommandType.CREATE_SERVICE_ACCOUNT,
+  //       params: { args: [] },
+  //     },
+  //     ...BASIC_MESSAGE,
+  //   };
+
+  //   await commandMessageProcessor(message, options);
+
+  //   expect(options.addBanner).to.have.been.calledWith('create_service_account_failed', []);
+  // });
+});
