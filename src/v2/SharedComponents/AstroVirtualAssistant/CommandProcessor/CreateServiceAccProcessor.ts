0cbef21806410fd9b59c6b272538df365ffd8a72
Justin Orringer <justinorringer@gmail.com>
2025-05-22 10:52:57 -0400

    Remove throwing error on auth

--- src/v2/SharedComponents/AstroVirtualAssistant/CommandProcessor/CreateServiceAccProcessor.ts
+++ src/v2/SharedComponents/AstroVirtualAssistant/CommandProcessor/CreateServiceAccProcessor.ts
@@ -3,9 +3,6 @@ import { PostCreateServiceAccRequest, postCreateServiceAcc } from '../../../api/
 import { MessageProcessorOptions } from '../../../Components/Message/MessageProcessor';
 
 export const createServiceAccProcessor = async (args: Record<string, string>, options: MessageProcessorOptions) => {
-  if (options.auth === null || options.auth === undefined) {
-    throw new Error('Auth is not defined');
-  }
   const requestPayload: PostCreateServiceAccRequest = {
     name: args.name,
     description: args.description,
